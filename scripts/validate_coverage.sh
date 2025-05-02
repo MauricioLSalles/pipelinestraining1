c6ba422e93b75c2b1b3d638d7afa6c243f7d0a10
MauricioLsalles <mauricio.leano@jala.university>
2025-05-02 09:22:05 -0400

    workflow test

--- scripts/validate_coverage.sh
+++ scripts/validate_coverage.sh
@@ -24,6 +24,8 @@ MIN_COVERAGE="0.8"
 
 COVERAGE_VALUE=$( echo $RATE | tr "." "\n" | sed -n '2p' | cut -c1-1 )
 
+echo COVERAGE_VALUE
+
 if [ "$COVERAGE_VALUE" -ge "$MIN_COVERAGE" ];
 then
     echo "Pass Successful"
